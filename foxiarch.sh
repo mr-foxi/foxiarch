c9bd4029c77024b1447a182253847921f8d51280
mr-foxi <fox.inta@proton.me>
2024-12-14 19:00:21 +1300

    Categorized with nvidia and audio improvements

--- foxiarch.sh
+++ foxiarch.sh
@@ -5,47 +5,67 @@ sudo pacman -Syyu --needed --noconfirm
 
 # Install packages from the official repositories
 sudo pacman -S --needed --noconfirm \
-base-devel \
-git \
-wine \
+# Gaming
 lutris \
 steam \
+# Tools
 openvpn \
+wine \
 lastpass-cli \
 code \
 docker \
 alacritty \
 gimp \
-nvidia-open \
-opencl-nvidia \
-nvidia \
-nvidia-utils \ 
-lib32-nvidia-utils \
-bumblebee \
-intel-compute-runtime \
-intel-opencl-runtime \
-intel-oneapi-compiler-shared-runtime \
-thunderbird \
-man-db \
-bluez \
-samba \
-remmina \
-retroarch \
-dolphin-emu \
-ppsspp \
-freerdp \
-libvncserver \
-gtk-vnc \
-spice-gtk \
 kwallet \
 kwalletmanager \
 ktorrent \
 partitionmanager \
-pipewire-media-session \
-pulseaudio-bluetooth \
-bluez-utils \
+thunderbird \
+man-db \
 scrcpy \
+# Nvidia Drivers
+nvidia \
+nvidia-open \
+opencl-nvidia \
+nvidia-utils \
+nvidia-settings \
+lib32-nvidia-utils \
+# Vulkan Drivers
+vulkan-icd-loader \
+lib32-vulkan-icd-loader \
+vulkan-tools \
+# Intel Drivers
+mesa intel-media-driver \
+lib32-mesa vulkan-intel lib32-vulkan-intel \
+intel-compute-runtime \
+intel-opencl-runtime \
+intel-oneapi-compiler-shared-runtime \
+# Hybrid Graphics Driver
+bumblebee \
+# Bluetooth Drivers
+bluez \
+bluez-utils \
+# Networking and Tools
+samba \
+remmina \
+freerdp \
+libvncserver \
+gtk-vnc \
+spice-gtk \
+# Emulation
+retroarch \
+dolphin-emu \
+ppsspp \
+# Audio Drivers
+pipewire-media-session \
+pipewire \
+pipewire-alsa \ 
+pipewire-jack \
+pipewire-pulse \
+# Desktop and Framework
 plasma \
+base-devel \
+git \
 qt6 \
 sof-firmware \
 xorg \
