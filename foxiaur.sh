d8f739c052d972a53a876e02927ce5d09bdec0ee
mr-foxi <fox.inta@proton.me>
2024-12-14 23:55:34 +1300

    Added razercfg
    razer mouse configuration tool

--- foxiaur.sh
+++ foxiaur.sh
@@ -19,3 +19,4 @@ yay -S --needed --noconfirm \
     vmware-workstation \
     rpcs3 \
     pcsx2 \
+    razercfg \ # It is important to only have Mouse and not Mouse# listed in /etc/X11/xorg.conf | file to disable the current mouse settings >><< maybe only for x11
